2707676ce8199fafe523b9a59573cf1bb04db2a5
Shaswetha Shankar <shaswethashankar@Shaswethas-MacBook-Pro.local>
2025-03-12 09:04:02 +0530

    Added hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,14 +1,2 @@
-//File: hello.cpp
 #include <iostream>
-using namespace std;
-
-int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
-  return 0;
-}
-std::cout << "Webhook Triggered!";
-std::cout << "New Webhook Test!";
-std::cout << "CI/CD Webhook Test Successful!";
-std::cout << "CI/CD Webhook Test Successful!";
+int main() { std::cout << "Hello, Jenkins!"; return 0; }
